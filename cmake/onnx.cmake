e084f8063fd8785a90c68f751ee3816bcde1967e
daquexian <daquexian566@gmail.com>
2019-04-15 10:59:37 +0800

    Make onnx2daq a standalone project

--- cmake/onnx.cmake
+++ cmake/onnx.cmake
@@ -1,6 +1,6 @@
 macro(configure_onnx)
     if (NOT ${DNN_SYSTEM_PROTOBUF})
-        include(${PROJECT_SOURCE_DIR}/cmake/protobuf.cmake)
+        include(${DNN_ROOT_DIR}/cmake/protobuf.cmake)
         configure_protobuf()
     endif()
 
@@ -8,7 +8,7 @@ macro(configure_onnx)
     set(DAQ_ONNX_NAMESPACE onnx_daq)
     
     set(ONNX_NAMESPACE ${DAQ_ONNX_NAMESPACE} CACHE STRING "onnx namespace")
-    add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/onnx)
+    add_subdirectory(${DNN_ROOT_DIR}/third_party/onnx third_party/onnx/)
     # Since https://github.com/onnx/onnx/pull/1318 is merged, we don't need to set it manually
     # target_compile_definitions(onnx
     # PUBLIC
