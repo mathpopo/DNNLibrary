c60e6cb7049e6b3cbc7d6ed5ad4e448e33015603
daquexian <daquexian566@gmail.com>
2019-04-17 12:25:59 +0800

    glog link static msvc runtime

--- cmake/glog.cmake
+++ cmake/glog.cmake
@@ -1,6 +1,9 @@
+include(${PROJECT_SOURCE_DIR}/cmake/utils.cmake)
+
 macro(configure_glog)
     message(STATUS "Configureing glog...")
     option(BUILD_TESTING "" OFF)
     option(WITH_GFLAGS "" OFF)
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/glog)
+    add_msvc_runtime_flag(glog)
 endmacro()
